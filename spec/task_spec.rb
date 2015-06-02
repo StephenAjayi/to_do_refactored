e0bd4d98fea5fad802bcab9975e875581c8dec75
stephen <lonelytreesl@yahoo.com>
2015-06-02 10:11:41 -0700

    add spec and code to Task#save to make it pass, all specs passing.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -3,7 +3,7 @@ require('task')
 
 describe(Task) do
   describe('#description') do
-    it('returns a description for a instance of a task') do
+    it('returns a description for a instance of a Task') do
       test_task = Task.new("Wash the car.")
       expect(test_task.description()).to(eq("Wash the car."))
     end
@@ -12,6 +12,14 @@ describe(Task) do
   describe('.all') do
     it('starts out empty at first') do
       expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves an instance of Task to an array') do
+      test_task = Task.new("Fix the windows.")
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
     end
   end
 end
